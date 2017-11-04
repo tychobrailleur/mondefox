6def881a692ceb17c44a0ee61df4ab6178bd03fa
Sébastien Le Callonnec <sebastien@weblogism.com>
2017-11-04 18:43:24 +0000

    Add localisation and use https for redirect on logo.

--- data/cleanup.js
+++ data/cleanup.js
@@ -6,30 +6,30 @@ var MondeFox = {
   CLASSES_TO_REMOVE: [ 'plus_partages' ],
 
   // Classes containing "img" to be reduced
-  IMGS_TO_REDUCE: ['titre_une','img_tt_chapo'],
+  IMGS_TO_REDUCE: [ 'titre_une','img_tt_chapo' ],
 
   removeUnwantedStuff: function(doc) {
-    for (var i in this.IDS_TO_REMOVE) {
+    for (let i in this.IDS_TO_REMOVE) {
       this.removeId(doc, this.IDS_TO_REMOVE[i]);
     }
-    for (var c in this.CLASSES_TO_REMOVE) {
+    for (let c in this.CLASSES_TO_REMOVE) {
       this.removeClass(doc, this.CLASSES_TO_REMOVE[c]);
     }
   },
 
   // Remove elements with a given class.
   removeClass: function(doc, className) {
-    var elements = doc.getElementsByClassName(className);
-    for (var el in elements) {
+    const elements = doc.getElementsByClassName(className);
+    for (let el in elements) {
       if (elements[el].parentNode) {
         elements[el].parentNode.removeChild(elements[el]);
       }
     }
   },
 
-  // Remove element with  specific id.
+  // Remove element with specific id.
   removeId: function (doc, idElement) {
-    var element = doc.getElementById(idElement);
+    const element = doc.getElementById(idElement);
     if (element) {
       element.parentNode.removeChild(element);
     }
@@ -37,26 +37,26 @@ var MondeFox = {
 
   // Make logo on home page clickable to reload homepage.
   makeLogoClickable: function(doc) {
-    var element = doc.getElementById("logo");
+    const element = doc.getElementById("logo");
     if (element) {
       element.style.cursor = "pointer";
       element.addEventListener("click", function(evt) {
-        window.location.href = "http://www.lemonde.fr";
+        window.location.href = "https://www.lemonde.fr";
       });
     }
   },
 
   // Make “chapeau” images a tad smaller
   makeChapeauImagesSmaller: function(doc) {
-    for (var classes in this.IMGS_TO_REDUCE) {
-      var elements = doc.getElementsByClassName(this.IMGS_TO_REDUCE[classes]);
+    for (let classes in this.IMGS_TO_REDUCE) {
+      let elements = doc.getElementsByClassName(this.IMGS_TO_REDUCE[classes]);
 
-      for (var i in elements) {
+      for (let i in elements) {
         var descendants = [];
         this.getDescendants(doc, elements[i], descendants);
-        for (var j in descendants) {
-          var current = descendants[j];
-          if (current.tagName == "IMG") {
+        for (let j in descendants) {
+          let current = descendants[j];
+          if (current.tagName === "IMG") {
             current.height = current.height / 2;
             current.width = current.width / 2;
           }
@@ -68,8 +68,8 @@ var MondeFox = {
   getDescendants: function(doc, element, descendants) {
     if (element.nodeType === Node.ELEMENT_NODE) {
       descendants.push(element);
-      for (var i in element.childNodes) {
-        var currentElement = element.childNodes[i];
+      for (let i in element.childNodes) {
+        let currentElement = element.childNodes[i];
         if (currentElement.nodeType === Node.ELEMENT_NODE) {
           if (currentElement.childNodes) {
             this.getDescendants(doc, currentElement, descendants);
